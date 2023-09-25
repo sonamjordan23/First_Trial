d2b842ed63ef34bd4214f52dcfaf74fb71497543
unknown <sonamjordan23@gmail.com>
2023-09-25 22:11:55 +0545

    4th commit

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 console.log("Hey! ✋ this is my first line of code");
-<<<<<<< HEAD
-
-console.log("Hey! ✋ this is my second line of code");
-
-console.log("Hey! ✋ this is my third line of code");
-=======
->>>>>>> parent of 52bbabe (2nd Commit)
